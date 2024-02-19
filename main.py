19b73e3ebe345c863a5cc24864f379bf8cb83cd8
Dale Tambis <clayd0423@gmail.com>
2024-02-19 17:56:49 +0800

    summation

--- main.py
+++ main.py
@@ -51,6 +51,12 @@ def operation(choice):
         print("Remainder: \t\t\t{}".format(remainder(x, y)))
         s.system("pause")
         s.system("cls")
+    elif choice == "f":
+        x = int(input("Enter the First number: \t"))
+        y = int(input("Enter the Second number: \t"))
+        print("Summation: \t\t\t{}".format(summation(x, y)))
+        s.system("pause")
+        s.system("cls")
     else:
         s.system("cls")
         print("Invalid choice")
@@ -62,6 +68,7 @@ while again == True:
     print("[D.] - Divide")
     print("[E.] - Exponentation")
     print("[R.] - Remainder")
+    print("[F.] - Summation")
     print("========================================")
     choice = input("Enter your choice: ").lower()
     s.system("pause")
